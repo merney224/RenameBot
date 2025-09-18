fd7d166ec9e5159838672bf7b832849ff5bbd933
merney224 <merney224@gmail.com>
2025-09-18 17:26:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5614990502:AAExXIrVBj_Q6UU8PmKOU5z5pF8j0WJC6ec")
-API_ID = int(os.environ.get("API_ID", "4297842"))
-API_HASH = os.environ.get("API_HASH", "bd495db96a52b1d9381657704e629b95")
-OWNER = int(os.environ.get("OWNER", "1502465002"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8152348968:AAGo_GiGkefoYH3pILeAN8T6aySP2nV4Oxk")
+API_ID = int(os.environ.get("API_ID", "19481274"))
+API_HASH = os.environ.get("API_HASH", "edbb4d162c4871c25ccd6f7179dad8cf")
+OWNER = int(os.environ.get("OWNER", "6585564185"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "CFrenamerbot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002537080415"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "CFUpdates")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002933718411"))
 
-DB_URL = os.environ.get("DB_URL", "mongodb+srv://lokah:lokah@lokah.hm66ghl.mongodb.net/?retryWrites=true&w=majority&appName=Lokah")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://variko4322_db_user:Neymarjr@cluster0.cdlal9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQCEW4EeuFlxOgnqbBx6xIZDCjhEI8WNVt29Z6AtLL-OozpUF6et-QEt1Ujrj3z8xBgPYqfdoNana1V5BXr9n1RRp6GRVygCNcHCeMfaMOguce1GT3sOFjsUehlIy4QSzLed3_zRC-zy6mgXBQDARi2ZHT6xoQS9J6nRsWEKoIOBHaFaQCM316Wfp2o4LkyGjs3JgLtYrUUA-w6daP6WE9bMRequzkNYXkvfh1nZZIK1sgg95evbhvbqe7gHlraKWR6fMZjxSAFEEJ8zhugE7wGHtC_qugGgZ6Hy4-ilbDz5dzt9s-AiPmTfJ4GKb0jQkXtYn8qZovmAaiakgTYV67miAAAAAYiHvBkA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
