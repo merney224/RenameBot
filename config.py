f62a5c83d303251d0369479f86b1b5b461f8a071
𝗧𝗚𝗘𝗕𝗢𝗧𝗭 <97307696+TGEBOTZ@users.noreply.github.com>
2025-09-18 14:48:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5614990502:AAExXIrVBj_Q6UU8PmKOU5z5pF8j0WJC6ec")
+API_ID = int(os.environ.get("API_ID", "4297842"))
+API_HASH = os.environ.get("API_HASH", "bd495db96a52b1d9381657704e629b95")
 OWNER = int(os.environ.get("OWNER", ""))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "1502465002")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://lokah:lokah@lokah.hm66ghl.mongodb.net/?retryWrites=true&w=majority&appName=Lokah")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
