2f14fe51cc3d8b79250b29e8df533f26bafa5e1e
𝗧𝗚𝗘𝗕𝗢𝗧𝗭 <97307696+TGEBOTZ@users.noreply.github.com>
2025-09-18 14:57:42 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os, re
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5614990502:AAExXIrVBj_Q6UU8PmKOU5z5pF8j0WJC6ec")
 API_ID = int(os.environ.get("API_ID", "4297842"))
 API_HASH = os.environ.get("API_HASH", "bd495db96a52b1d9381657704e629b95")
-OWNER = int(os.environ.get("OWNER", ""))
+OWNER = int(os.environ.get("OWNER", "1502465002"))
 BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "1502465002")
